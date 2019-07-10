92dc1fbfdc5294841e5b24b295073a9949d15e69
2742396435 <2742396435@qq.com>
2019-07-10 15:26:27 +0800

    青青蛙

---
+++ qingwa.php
@@ -0,0 +1,18 @@
+<?php
+    function number($n)
+    {
+        if($n>=3)
+        {
+            return number($n-1)+number($n-2);
+        }
+        elseif ($n==1)
+        {
+            return 1;
+        }
+        elseif ($n==2)
+        {
+            return 2;
+        }
+    }
+    echo number($n=9);
+?>
