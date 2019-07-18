a5fa0ed710a1d0c3de0d80caf865925998db3ef1
2742396435 <2742396435@qq.com>
2019-07-18 08:43:43 +0800

    日考11

---
+++ daoxu.php
@@ -0,0 +1,13 @@
+<?php 
+	$str = "student. a am I";
+	function get($str)
+	{
+		$arr = explode(" ", $str);
+		krsort($arr);
+		$arr = implode(" ", $arr);
+		return $arr;
+	}
+	print_r(get($str));
+
+
+ ?>
