37e05f48e18bd1bdbf9ad412dbae3dd75e6a11ee
2742396435 <2742396435@qq.com>
2019-07-25 08:46:36 +0800

    日考17

---
+++ 17.php
@@ -0,0 +1,14 @@
+<?php 
+		///echo Combine($arr_A,$arr_B);
+		$arr_A=[1,3,6,9];
+		$arr_B=[2,4,5,8,20];
+		function Combine()
+		{
+			//count_sum($arr_A=[1,3,6,9]+$arr_B=[2,4,5,8,20]);
+			$arr_A=[1,3,6,9];
+			$arr_B=[2,4,5,8,20];
+		}
+		var_dump([1,2,3,4,5,6,8,9,20]);
+
+
+ ?>
