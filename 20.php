1555597e7554088ccfa7cfd02d2a810effac9177
2742396435 <2742396435@qq.com>
2019-07-29 08:48:27 +0800

    日考20

---
+++ 20.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	CountSteps($x,$y);
+
+	function arr()
+	{
+
+		foreach ($list as $k => $v) {
+		if ($x=1,$y=1) {
+			$arr = substr_count(CountSteps, 1);
+		}
+	}
+	}
+	
+	return arr;
+
+
+ ?>
